6ca2e49ded71a7f3c1061e170bb26ed960080105
loster12520 <xiaoshuo12520@outlook.com>
2025-02-25 16:33:33 +0800

    fix some bugs in edit view save function

--- app/src/views/ArticleEdit.vue
+++ app/src/views/ArticleEdit.vue
@@ -172,34 +172,42 @@ onMounted(() => {
       <p class="Text">快给我保存！！</p>
       <p class="Text">哼！</p>
     </a-modal>
-<!--    <a-form-->
-<!--        name="basic"-->
-<!--        :label-col="{ span: 8 }"-->
-<!--        :wrapper-col="{ span: 16 }"-->
-<!--        autocomplete="on"-->
-<!--        @finish="onFinish"-->
-<!--        @finishFailed="onFinishFailed"-->
-<!--    >-->
-<!--      <a-form-item-->
-<!--          label="Username"-->
-<!--          name="username"-->
-<!--          :rules="[{ required: true, message: 'Please input your username!' }]"-->
-<!--      >-->
-<!--        <a-input v-model:value="formState.username"/>-->
-<!--      </a-form-item>-->
+    <a-form
+        name="basic"
+        :label-col="{ span: 8 }"
+        :wrapper-col="{ span: 16 }"
+        autocomplete="on"
+        @finish="onFinish"
+        @finishFailed="onFinishFailed"
+    >
+      <a-form-item
+          label="Title"
+          name="title"
+          :rules="[{ required: true, message: 'Your title!' }]"
+      >
+        <a-input v-model:value="article.title"/>
+      </a-form-item>
 
-<!--      <a-form-item-->
-<!--          label="Password"-->
-<!--          name="password"-->
-<!--          :rules="[{ required: true, message: 'Please input your password!' }]"-->
-<!--      >-->
-<!--        <a-input-password v-model:value="formState.password"/>-->
-<!--      </a-form-item>-->
+      <a-form-item
+          label="Author"
+          name="author"
+          :rules="[{ required: true, message: 'Your author!' }]"
+      >
+        <a-input v-model:value="article.author"/>
+      </a-form-item>
 
-<!--      <a-form-item :wrapper-col="{ offset: 8, span: 16 }">-->
-<!--        <a-button type="primary" html-type="submit">Submit</a-button>-->
-<!--      </a-form-item>-->
-<!--    </a-form>-->
+      <a-form-item
+          label="Description"
+          name="description"
+          :rules="[{ required: true, message: 'Your description!' }]"
+      >
+        <a-input v-model:value="article.description"/>
+      </a-form-item>
+
+      <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+        <a-button type="primary" html-type="submit">Submit</a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
